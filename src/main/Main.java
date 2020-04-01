de4d0f11d03864f7b85210148e164c7a539c5dc9
tonybricheff <tony.bricheff@yandex.ru>
2020-04-01 18:42:56 +0300

    added some code refactor

---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+import main.InternshipTask;
+
+public class Main {
+    public static void main(String[] args) {
+        InternshipTask solution = new InternshipTask();
+        solution.solve();
+    }
+}
